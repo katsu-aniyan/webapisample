31f2a0af8f4250a1d457da7694f33b664250f544
Katsushige Aniya <katsu.aniyan@gmail.com>
2017-04-12 02:11:26 +0900

    first commit;

---
+++ src/zip/api.js
@@ -0,0 +1,27 @@
+
+//$("button[data-btn-type='ajax']")[0]
+
+//http://zipcloud.ibsnet.co.jp/api/search?zipcode=2500011
+$(function(){
+	var send_data = {};
+	$('body').on('click', 'button[data-btn-type=ajax]', function(){
+		console.log('click btn');
+		$.ajax({
+			url: 'http://zipcloud.ibsnet.co.jp/api/search?zipcode=2500011',
+			dataType: "json",
+			data : send_data,
+			success: function(res){
+				if(res.result === 'OK') {
+					console.log(res);
+				} else {
+					console.log(res);
+				}
+				return false;
+			},
+			error: function(){
+				console.log("error occured");
+			}
+		});
+	});
+});
+
